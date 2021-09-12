9153555bfcd25a9d56a972a323839c9f168854a9
Terr1bus <qwant94@mail.ru>
2021-09-12 14:36:21 +0300

    fix: rename getActivityStatistic folder

--- be/src/functions/activities/index.ts
+++ be/src/functions/activities/index.ts
@@ -1,4 +1,4 @@
 export { getActivities } from './getActivities';
 export { addActivity } from './addActivity';
-export { getActivityStatistic } from './getStatistics';
+export { getActivityStatistic } from './getActivityStatistic';
 export { dailyPenalty } from './dailyPenalty';
